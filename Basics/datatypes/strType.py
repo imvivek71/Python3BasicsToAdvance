520726ee93777a30ee7d772626eaca021c639432
imvivek71 <vivekgoswami71@gmail.com>
2019-02-06 18:01:03 +0530

    ...

---
+++ Basics/datatypes/strType.py
@@ -0,0 +1,31 @@
+"""""
+String is as sequence of  characters enclosed by ' or   "
+"""
+a = 'vivek'
+
+b = "vivek"
+
+# Multi line  String by using  triple single  / double quotes
+
+c = """
+vivek
+Goswami
+"""
+print(a,' ', b, ' ' , c  )
+
+# Slicing of strings(piece of a sting )
+
+# By using  +ve index fro left to right (0 to n-1)
+print(a[0])
+
+# By using negative index from right to left (-1 to n)
+
+print(a[-1])
+print( a[:] ) # will print all elements
+print(a[0:-1]) # will print n-1 elements
+print(a*4) # 4 times repeat
+print(len(a)) # Length of str
+
+
+
+
