8103be469db5e428209d09c85c789ead24b2bdfb
imvivek71 <vivekgoswami71@gmail.com>
2019-02-06 16:31:12 +0530

    ...

---
+++ Basics/KeyWords.py
@@ -0,0 +1,7 @@
+# To print the all keywords of python.
+# Keywords  contains  alphabets only
+# Except  True False None each contains  lowercase only
+
+
+import keyword
+print(keyword.kwlist)
---
+++ Basics/datatypes/datatype.py
@@ -0,0 +1,9 @@
+"""""
+There are 14  inbuilt data types:
+int, float,complex, bool, str, bytes, bytearray, range, list, tuple, set, frozenset, dict, None
+"""
+a = 10
+#  to check  the type of  variable
+print(type(a))
+# to get the address of object
+print(id(a))
