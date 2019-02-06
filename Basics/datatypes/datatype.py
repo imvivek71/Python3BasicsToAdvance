da752f0c0d6a39057168116c896c46d2fdee15d2
imvivek71 <vivekgoswami71@gmail.com>
2019-02-06 18:02:09 +0530

    ...

--- Basics/datatypes/datatype.py
+++ Basics/datatypes/datatype.py
@@ -1,6 +1,8 @@
 """""
 There are 14  inbuilt data types:
 int, float,complex, bool, str, bytes, bytearray, range, list, tuple, set, frozenset, dict, None
+
+ & int, float,complex, bool, str are fundamental data types in python
 """
 a = 10
 #  to check  the type of  variable
