45f89e341cb3e268ec48dceb7d5744eb82ba54a4
imvivek71 <vivekgoswami71@gmail.com>
2019-02-06 22:01:23 +0530

    ...

---
+++ Basics/datatypes/dictdataType.py
@@ -0,0 +1,11 @@
+"""""
+To represent a group  of values as key value-pairs we use dict
+
+order not supported as d[0]
+
+"""
+d = {101:'name', 102:'class',102:'year'}
+d[101] = 'vivek'
+d[105] = 'am'
+d[106]  = 'mango'
+print(d)
