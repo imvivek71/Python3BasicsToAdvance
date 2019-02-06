47efb9b2a7446a11d008f704fa4991df125664c5
imvivek71 <vivekgoswami71@gmail.com>
2019-02-06 21:22:52 +0530

    ...

---
+++ Basics/datatypes/tuple.py
@@ -0,0 +1,12 @@
+"""""
+It is similar to list, but is immutable
+
+So it does not support append remove & item assignment
+
+t = ()
+"""
+
+t = (10,12,'vivek',13)
+print(type(t))
+
+# It is the read only version of the list
