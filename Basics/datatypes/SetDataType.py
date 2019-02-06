e675a04d3917e2f5faefab5da2ce9ded52aceaaa
imvivek71 <vivekgoswami71@gmail.com>
2019-02-06 21:49:08 +0530

    ...

---
+++ Basics/datatypes/SetDataType.py
@@ -0,0 +1,15 @@
+"""""
+Mutable
+Duplicates are not allowed
+Heterogeneous are allowed
+Index concept is not applicable
+Growable in nature by using add
+
+
+
+"""
+
+s = {10,12,'vivek',13}
+s.add(20)
+s.remove(12)
+print(s)
