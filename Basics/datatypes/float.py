fd2cb15daffb3f73b915f14cfd7c773a982582dd
imvivek71 <vivekgoswami71@gmail.com>
2019-02-06 17:07:50 +0530

    ...

---
+++ Basics/datatypes/float.py
@@ -0,0 +1,10 @@
+# To represent floating  type values
+f= 1.1233445
+print(f)
+# we can also use by using scientific notations  "due to this we can represent big values in less memory"
+
+a = 1.2e4
+b = 1.2E4
+print(a, ' ', b)
+
+# We can only represent float values  only by using decimal form
