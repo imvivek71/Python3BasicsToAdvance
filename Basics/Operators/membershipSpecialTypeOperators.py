6792b43e77a347ff8c8459406c750f5c838c8f18
imvivek71 <vivekgoswami71@gmail.com>
2019-02-08 00:52:12 +0530

    ...

---
+++ Basics/Operators/membershipSpecialTypeOperators.py
@@ -0,0 +1,12 @@
+"""""
+We can use the membership operator to check whether the object present in the given collection(str, list, tuple, stt or dict)
+
+
+It will return true if the object  present in that
+"""
+a = 'Vivek is a great man'
+list = [10,13,14]
+c =10
+
+print('v' in a)  # True
+print(c in list)  # True
