c8e7048b34d2017ce50abe5ab8981fd7df3d3a9f
imvivek71 <vivekgoswami71@gmail.com>
2019-02-08 03:32:05 +0530

    ...

---
+++ InputOutput/basicOfIO.py
@@ -0,0 +1,13 @@
+#  input() in python 3 is replacement of  raw_input() of python 2
+
+#  input() function will take all data input in string form
+
+z = input()
+
+a = list(input())
+b = int(input())
+c = float(input())
+d = complex(input())
+x = bool(input())
+
+print(a,b,c,d,x,z)
