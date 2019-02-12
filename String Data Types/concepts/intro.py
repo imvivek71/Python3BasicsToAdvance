5cd2174e716bdd0234a7c898a3c0dcd7e7661ede
imvivek71 <vivekgoswami71@gmail.com>
2019-02-12 21:11:48 +0530

    ...

---
+++ String Data Types/concepts/intro.py
@@ -0,0 +1,33 @@
+"""""
+String:  Any sequence of characters within either single quotes or double quotes
+Properties of string: Immutable
+
+"""
+# Example
+string = 'by using single quote'
+new = "by using double quote"
+
+
+# Multi line strings
+
+multi = """
+multi enclosed 
+by 3 double quote
+"""
+
+multi1 = '''
+multi enclosed 
+by 3 single quote
+
+'''
+print(string,"," ,new,  multi, multi1)
+
+
+# Accessing the characters of string
+# by using index
+
+print(string[0], string[1],string[-1])
+
+# By using slicing
+
+
