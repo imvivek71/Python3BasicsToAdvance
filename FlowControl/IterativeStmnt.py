63d7ecee3fc5cff6ef7562080d019f7c3602f281
imvivek71 <vivekgoswami71@gmail.com>
2019-02-12 20:03:03 +0530

    ...

--- FlowControl/IterativeStmnt.py
+++ FlowControl/IterativeStmnt.py
@@ -11,5 +11,26 @@ x = input('enter a string')
 for i in x:
     print(i,end=' ')
 
-while x<=10:
+while x<='10':
     print()
+
+
+
+
+# Nested loop
+
+for i in range(10):
+    for j in range(i):
+        print('*', end='')
+    print()
+
+
+# Infinite loop
+k = input('enter')
+
+
+while True:
+    print("Infinite loop")
+
+
+
