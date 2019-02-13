62fd4c24bad2962531bffa89416ed1f9ccd9f42b
imvivek71 <vivekgoswami71@gmail.com>
2019-02-13 23:03:47 +0530

    ...

---
+++ String Data Types/concepts/StartEndString.py
@@ -0,0 +1,10 @@
+#  Checking start and end point of a string
+
+
+s = 'i am a great programmer'
+
+print(s.startswith('great'))  # to check whether the parent string start with given substring
+
+print(s.endswith('er'))    # to check whether the parent string ends with given substring
+
+
