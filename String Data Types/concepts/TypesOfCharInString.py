d976fb316619cacd27c456311f4623a3bf22a3b7
imvivek71 <vivekgoswami71@gmail.com>
2019-02-13 23:33:47 +0530

    ...

--- String Data Types/concepts/TypesOfCharInString.py
+++ String Data Types/concepts/TypesOfCharInString.py
@@ -4,9 +4,9 @@
 isalnum()    to check all characters are alphanumerics ore not  (a-z,A-Z,0 to 9)
 isdigit()    to check all characters are only digits only(0 to 9)
 isalpha()    to check all characters are only alpha(a-z or A to Z)
-islower()    to check all characters are in lowercase
-isupper()    to check all characters are in uppercase
-istitle()    to check all characters are in title case
+islower()    to check all characters presented in the str are in lowercase,  string can contain anytype of char
+isupper()    to check all characters are in uppercase, string can contain anytype of char
+istitle()    to check all characters are in title case, string can contain anytype of char
 isspace()    to check all characters are spaces only
 
 """
---
+++ String Data Types/concepts/formatting.py
@@ -0,0 +1,9 @@
+#  We can format the string  by using replacement operator {} and format()
+
+name = 'Vivek'
+salary = 23000
+experience = 1
+
+print('{} is having {}  year of experience and his  salary is {} INR'.format(name,experience,salary))
+
+print('{x} is having {y}  year of experience and his  salary is {z} INR'.format(x =name,y =experience,z=salary))
