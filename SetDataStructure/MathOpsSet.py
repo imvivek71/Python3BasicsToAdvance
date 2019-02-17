9468ce9c566015bf3fbf112db4641d8c1ac37843
imvivek71 <vivekgoswami71@gmail.com>
2019-02-17 21:02:20 +0530

    ...

--- SetDataStructure/MathOpsSet.py
+++ SetDataStructure/MathOpsSet.py
@@ -1,10 +1,44 @@
 # union  to return all the elements present in both sets
 
-x =set(range(1,10,2))
-y = set(range(10,20,2))
+x =set(range(0,10,2))
+y = set(range(6,20,2))
 print(x|y)
 print(x.union(y))
 
 
-# intersection of x&y
+# intersection of x&y will return all the common elements present in both the set
+
+print(x.intersection(y))  # or
+
+print(x&y)
+
+# difference x-y returns the elements present in x but not in y
+
+print(x.difference(y))
+
+print(x-y)
+print(y.difference(x))
+print(y-x)
+
+
+# symmetric difference uncommon elements in both
+
+print(x.symmetric_difference(y))
+print(x^y)
+
+
+# Membership Operator in,  not in
+
+print(10 in x)
+print(10 not in x)
+
+# Set comprehensions
+
+s = {z*z for z in range(1,10)}
+print(s)
+
+
+s = {c**2 for c in range(1,10,2)}
+
+print(s)
 
