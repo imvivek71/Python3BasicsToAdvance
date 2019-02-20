651049e571b52a721e24a7c91b39c6db5ce5f50f
imvivek71 <vivekgoswami71@gmail.com>
2019-02-20 16:37:00 +0530

    ...

--- PatternFun/Pattern3.py
+++ PatternFun/Pattern3.py
@@ -1,18 +1,19 @@
 """""
-*****
-****
-***
-**
-*
+
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
 """
 
 x = int(input('Enter a num' ))
 
 for i in range(x+1):
-    for j in range(x - i):
-        print(' ', end='')
+    for k in range(x-i):
+        print(' ',end='')
 
-    for j in range(x-i):
-        print('*',end='')
-
+    for j in range(i):
+        print(' *',end='')
     print()
