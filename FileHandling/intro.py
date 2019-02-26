4c2fa98648af09ea7e79eea7820d758ae9031f98
imvivek71 <vivekgoswami71@gmail.com>
2019-02-26 22:07:30 +0530

    User Defined Exception

---
+++ FileHandling/intro.py
@@ -0,0 +1,20 @@
+"""""
+To perform file related I/O operations we use this
+
+
+"""
+
+f = open('abc.txt','w') # this will open the file for write option, if file available then its cool, otherwise this will create a file
+f.write('Vivek ')     # in this form data is considered as string form only
+f.write('Kumar \n')
+f.write('Goswami')
+f.close()
+
+
+x = open('abc.txt') # reading data
+data = x.read()
+print(data)
+
+
+
+
