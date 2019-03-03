bde6e8a26307ebd8dade49e6e62b7c2fd0584c6c
imvivek71 <vivekgoswami71@gmail.com>
2019-03-03 18:17:56 +0530

    User Defined Exception

--- functions/keywordArguments.py
+++ functions/keywordArguments.py
@@ -12,3 +12,9 @@ KeywordArgv(y = 10,x = 34)  # Here order of argument does not matter
 
 KeywordArgv(10, y=11)  # This will work because because simple argument can follow keyword or default argument
 
+def Fun2(vivek, radhe = 'Ritesh'):
+    print(vivek, radhe)
+#  Fun2(vivek='Varsha', 'ganesh')  # non keyword or non default argument are come before keyword/default argument
+
+Fun2(10, 'zk')
+
