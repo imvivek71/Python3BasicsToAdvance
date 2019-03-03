0dcb05450ef6b78bba4a5a542b08fba63e74bef2
imvivek71 <vivekgoswami71@gmail.com>
2019-03-03 19:15:12 +0530

    User Defined Exception

--- functions/VariableLengthArguments.py
+++ functions/VariableLengthArguments.py
@@ -1,24 +1,26 @@
 #   We can have both normal and keyword  variable number arguments for only multiple we will take *args
 #   and for both **kwargs
 
+def MyFun(m =10,*n):      #  we can give arguments with *args
 
-def MyFun(*n):
     for items in n:
         print(items,end=' ')
 
-
+MyFun(12,2,23,43)
 MyFun(10,12,2,23,43)
-MyFun(10,12,2,23,43)
 
 
-
-
-def MyFun1(**n):
+def MyFun1(m = 18,**n):
     for items in n.items():
         print(items)
+        print(m)
 
 MyFun1(x = 'Vivek',y = 'Varsha', z = 'devloper')
 
 # O/P  ('x', 'Vivek') ('y', 'Varsha') ('z', 'devloper')
 
 #  MyFun1(10,x = 'Vivek',y = 'Varsha', z = 'devloper')  This does not take any positional argument
+
+
+def MyFun3(m,*n):   # this is also valid
+   pass
