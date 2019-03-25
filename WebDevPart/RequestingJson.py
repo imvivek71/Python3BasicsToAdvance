eb437c89e0268789f7e55e0dc13085c38fb1bb12
imvivek71 <vivekgoswami71@gmail.com>
2019-03-26 02:24:02 +0530

    Bubble Sort

---
+++ WebDevPart/RequestingJson.py
@@ -0,0 +1,15 @@
+# JSON is javascript object notation
+
+
+import requests
+
+response = requests.get('https://icanhazdadjoke.com/', headers  = {"Accept": "application/json"})  # for plain tect string formta
+
+
+print(response.text)   # this will give the output in string format
+
+print(type(response.text))  # class str
+
+print(response.json())  # This will give the output in dictionary fromat
+
+print(type(response.json()))
